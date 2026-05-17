ae329b15b8cc9b2f5bf07ed092d3eae5ec997e70
DTA32 <97021335+dta32@users.noreply.github.com>
2026-05-18 00:54:58 +0700

    ci: setup runnable

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,23 @@
 import { defineConfig } from 'vite'
 import { devtools } from '@tanstack/devtools-vite'
-
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-
 import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-const config = defineConfig({
+const config = defineConfig(({ mode }) => ({
   resolve: { tsconfigPaths: true },
-  plugins: [devtools(), tailwindcss(), tanstackStart(), viteReact()],
-})
+  plugins: [
+    tanstackStart({
+      server: {
+        build: {
+          staticNodeEnv: true,
+        },
+      },
+    }),
+    tailwindcss(),
+    mode !== 'production' ? devtools() : null,
+    viteReact(),
+  ],
+}))
 
 export default config
